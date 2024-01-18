2cad83b4bb60c50d5cd2af4f799c1ee6a771ee69
Carlos Villagran <cjvillagranmata@gmial.com>
2024-01-18 08:57:42 -0600

    Tablero commit

--- src/Tablero.java
+++ src/Tablero.java
@@ -11,8 +11,13 @@ public class Tablero {
                         + "*                    Radio                        *\n"
                         + "**************************************************\n"
                         + "* 1.  encender radio                             *\n"
-                        + "* 2.                                             *\n"
-                        + "* 3.                                             *\n"
+                        + "* 2.  apagar radio                               *\n"
+                        + "* 3.  set Volumen                                *\n"
+                        + "* 4.  cambiar banda                              *\n"
+                        + "* 5.  subir emisora                              *\n"
+                        + "* 6.  bajar emisora                              *\n"
+                        + "* 7.  guardar emisora                            *\n"
+                        + "* 5.  seleccionar emisora guardada (1-12)        *\n"
                         + "**************************************************\n");
                 opc = Integer.parseInt(getNumber(in));
 
@@ -21,21 +26,60 @@ public class Tablero {
                         radio.encender();
                         break;
 
-                    case 2: //
-
+                    case 2: //Apaga la radio, saca del tablero
+                        radio.apagar();
+                        System.out.println("Ha apagado la radio");
                         break;
 
-                    case 3: //salir
-                        System.out.println("Gracias por su tiempo");
+                    case 3: //elegir volumen
+                        System.out.println("Ingrese el volumen deseado de 1 a 100");
+                        int vol = Integer.parseInt(getNumber(in));
+                        radio.setVolume(vol);
+                        System.out.println("Volumen en " + radio.getVolumen());
                         break;
 
+                    case 4: //cambiar banda
+                        System.out.println("Elija una banda AM=0, FM=1");
+                        int banda = Integer.parseInt(getNumber(in));
+                        radio.cambiarBanda(banda);
+                        System.out.println("Estás escuchando " + radio.getBanda());
+                        break;
+
+                    case 5: //Subir emisora
+                        radio.subirEmisora();
+                        System.out.println("Estas escuchando" + radio.getEmisora());
+                        break;
+
+                    case 6: //Bajar emisora
+                        radio.bajarEmisora();
+                        System.out.println("Estas escuchando" + radio.getEmisora());
+                        break;
+
+                    case 7: //Guardar emisora
+                        System.out.println("Escriba el slot en el que quiere guardar la emisora (1-12)");
+                        int id = Integer.parseInt(getNumber(in));
+                        System.out.println("Ingrese la emisora que desea guardar");
+                        float emi = Float.parseFloat(getNumber(in));
+                        radio.guardarEmisora(id, emi);
+                        break;
+
+                    case 8: //Elegir emisora guardada
+                        System.out.println("Escriba el slot en el que quiere guardar la emisora (1-12)");
+                        int id2 = Integer.parseInt(getNumber(in));
+                        radio.seleccionarEmisora(id2);
+                        System.out.println("Emisora en " + radio.getEmisora());
+                        break;
+
+                    case 9: //salir
+
+                        break;
 
                     default:
                         System.out.print("Esta opción no es válida por favor reintentar \n");
                         break;
                 }
 
-            }while(opc != 3);
+            }while(opc != 2);
         } catch (NumberFormatException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
